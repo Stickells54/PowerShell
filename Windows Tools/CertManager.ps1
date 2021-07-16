b23632aa56c47a584154c414a6fa434849101eae
Stickells54 <59737454+Stickells54@users.noreply.github.com>
2021-07-16 09:34:37 -0400

    Update CertManager.ps1

--- Windows Tools/CertManager.ps1
+++ Windows Tools/CertManager.ps1
@@ -17,7 +17,7 @@
 	[Parameter(Mandatory = $true)]
 	$Org,
 	[Parameter(Mandatory = $true)]
-	$Country = 'US',
+	$Country = 'US'
 	
 )
 
@@ -95,7 +95,7 @@ function DownloadCSRTemplate
 		Write-Host "Path Created as it did not already exist." -ForegroundColor Green
 	}
 	Write-Host "Copying Template to location"
-	Invoke-WebRequest -Uri "https://github.com/Stickells54/PowerShell/blob/master/Windows%20Tools/csrtemplate.csv" -OutFile "$SaveLocation\csrtemplate.csv"
+	Invoke-WebRequest -Uri "https://raw.githubusercontent.com/Stickells54/PowerShell/master/Windows%20Tools/csrtemplate.csv" -OutFile "$SaveLocation\csrtemplate.csv"
 	if (!(Test-Path -Path $SaveLocation\csrtemplate.csv)) { Write-Host "Copy failed" }
 }
 
@@ -177,7 +177,7 @@ function ImportExport
 {
 	Write-Host "This will attempt to perform the Import/Export function on all certs in the directory you specify. To avoid errors, use a directory that ONLY has the CER files you need." -ForegroundColor Yellow -BackgroundColor Black
 	Write-Host "Make sure the format of the CER files are CN.cer. The script uses the filename to determine the CN to perform the Exporting correctly." -ForegroundColor Yellow -BackgroundColor Black
-	$CertsLocation = Read-Host -Prompt "Enter the folder where your CRT files are stored i.e C:\CER: "
+	$CertsLocation = Read-Host -Prompt "Enter the folder where your CER files are stored i.e C:\CER: "
 	$ExportLocation = Read-Host -Prompt "Enter the folder where you want the exported certificates to be created ex C:\Certs\Exported: "
 	$PFXPWD = Read-Host -Prompt "Enter a password for the PFX file: "
 	$PFXPWDENCRYPT = ConvertTo-SecureString -String $PFXPWD -Force -AsPlainText
@@ -193,7 +193,7 @@ function ImportExport
 	{
 		Import-Certificate -CertStoreLocation Cert:\LocalMachine\My\ -FilePath $CertsLocation\$Cert | Out-Null
 		$CN = $Cert.Substring(0, $Cert.length - 4)
-		$CertNames = $CertNames + $CN
+		$CertNames += $CN
 	}
 	Write-Host "Certs Imported. Beginning Export Process.."
 	#This part is convulated kind of, so I will try and explain as I go
@@ -203,13 +203,6 @@ function ImportExport
 		$CERTTP = (dir Cert:\LocalMachine\My\ -Recurse | ? Subject -Like "*CN=$($Cert)*").Thumbprint | Out-Null ### This finds the certificate in the Personal Directory that has a CN matching that if the CN provided in the CertNames array. This is a foreach loop so it does this for each cert in the array   
 		New-Item -Path $ExportLocation\$Cert -ItemType Directory -Force | Out-Null ## Create a folder in the directory provided with the CN of each cert. This is where the PFX files will be stored.
 		Get-ChildItem -Path Cert:\LocalMachine\my\$($CERTTP) | Export-PfxCertificate -Password $PFXPWDENCRYPT -FilePath $ExportLocation\$Cert\$Cert.pfx | Out-Null ## Export the cert we installed earlier in the function to a PFX with the password given by the user
-	}
-	## Check if OpenSSL is in the correct directory of C:\OpenSSL. Copies it to the machine if now
-	$OpenSSLTest = Test-Path -Path C:\OpenSSL
-	if ($OpenSSLTest -eq $false)
-	{
-		New-Item -Path C:\OpenSSL -ItemType Directory
-		Copy-Item -Path "\\rlghncsxvum3.devsub.dev.dce.usps.gov\team_vdi\Certificate Repository\OpenSSL\*" -Recurse -Destination C:\OpenSSL
 	}
 	#Run OpenSSL Command to generate PEM,CER, and KEY files from PFX
 	foreach ($pfxcert in $CertNames)
@@ -221,6 +214,9 @@ function ImportExport
 		Start-Process -FilePath C:\OpenSSL\openssl.exe -ArgumentList "pkcs12 -in $Cert -passin pass:$PFXPWD -out $ExportLocation\$pfxcert\$pfxcert.crt -nokeys" -NoNewWindow
 		#Convert existing PEM to Key file
 		Start-Process -FilePath C:\OpenSSL\openssl.exe -ArgumentList "rsa -in $ExportLocation\$pfxcert\$pfxcert.pem -out $ExportLocation\$pfxcert\$pfxcert.key -nodes" -NoNewWindow
+		#Regenerate PFX from CRT and Key
+		Start-Process -FilePath C:\OpenSSL\openssl.exe -ArgumentList "pkcs12 -export -out $ExportLocation\$pfxcert\$pfxcert-PFX.pfx -password pass:$PFXPWD -inkey $ExportLocation\$pfxcert\$pfxcert.key -in $ExportLocation\$pfxcert\$pfxcert.crt " -NoNewWindow
+		
 	}
 }
 
